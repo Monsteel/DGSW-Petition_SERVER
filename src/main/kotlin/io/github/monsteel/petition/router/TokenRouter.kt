32deec30c5870756c1429f6f67836dff1e98bcf9
이영은 <52942409+Monsteel@users.noreply.github.com>
2021-04-15 09:54:50 +0900

    Feature Token Refresh (#7)

---
+++ src/main/kotlin/io/github/monsteel/petition/router/TokenRouter.kt
@@ -0,0 +1,22 @@
+package io.github.monsteel.petition.router
+
+import io.github.monsteel.petition.handler.TokenHandler
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.server.RequestPredicates
+import org.springframework.web.reactive.function.server.RouterFunctions
+import org.springframework.web.reactive.function.server.router
+
+@Configuration
+class TokenRouter(
+    private val handler: TokenHandler
+) {
+    @Bean
+    fun routerAuth() = RouterFunctions.nest(RequestPredicates.path("/token"),
+        router {
+            listOf(
+                POST("/refresh", handler::refreshToken),
+            )
+        }
+    )
+}
