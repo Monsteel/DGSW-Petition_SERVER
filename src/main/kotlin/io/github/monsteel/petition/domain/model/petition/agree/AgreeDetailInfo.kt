1554e7abf14daa887ddd61504ee9f9735cb0a119
이영은 <eun36739@gmail.com>
2021-05-28 11:10:41 +0900

    [FIX] DateFormat(ISO8601)

--- src/main/kotlin/io/github/monsteel/petition/domain/model/petition/agree/AgreeDetailInfo.kt
+++ src/main/kotlin/io/github/monsteel/petition/domain/model/petition/agree/AgreeDetailInfo.kt
@@ -1,19 +1,20 @@
 package io.github.monsteel.petition.domain.model.petition.agree
 
+import io.github.monsteel.petition.util.extension.toISOString
 import java.util.*
 
 class AgreeDetailInfo {
     var idx: Long? = null
     var petitionIdx: Long
     var userID: String
-    var createdAt: Date
+    var createdAt: String
     var content:String
 
     constructor(idx: Long, petitionIdx: Long, userID: String, createdAt: Date, content: String) {
         this.idx = idx
         this.petitionIdx = petitionIdx
         this.userID = userID
-        this.createdAt = createdAt
+        this.createdAt = createdAt.toISOString()
         this.content = content
     }
 }
